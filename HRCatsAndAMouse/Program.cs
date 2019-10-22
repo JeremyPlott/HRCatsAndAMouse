1cf3013d8609e2d375c4a302bee6835dcc276305
Jeremy <JeremyPlott@gmail.com>
2019-10-22 13:38:35 -0400

    it good

--- HRCatsAndAMouse/Program.cs
+++ HRCatsAndAMouse/Program.cs
@@ -1,11 +1,32 @@
-﻿using System;
+﻿using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
 
 namespace HRCatsAndAMouse {
     class Program {
         static void Main(string[] args) {
 
+            var catADist = Math.Abs(z - x);
+            var catBDist = Math.Abs(z - y);
 
-
+            if(catADist == catBDist) {
+                Console.WriteLine("Mouse C");
+            }
+            if(catADist > catBDist) {
+                Console.WriteLine("Cat B");
+            } else {
+                Console.WriteLine("Cat A");
+            }
         }
     }
 }
